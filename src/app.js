1eaff1ff1fe2d3ff42be9b7924ce4fc3409d5f43
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-06-23 13:51:12 +0300

    Add mouse movement to game pad

--- src/app.js
+++ src/app.js
@@ -5,9 +5,15 @@ const canvas = document.querySelector('#canvas');
 /** @type {CanvasRenderingContext2D} */
 export const ctx = canvas.getContext('2d');
 
+export const canvasWidth = 800;
+export const canvasHeight = 600;
 export const brickWidth = 50;
 export const brickHeight = 15;
-export const padWith = 100;
+export const padWidth = 100;
 export const padHeight = 20;
+export const mouse = { x: 0, y: 0 };
 
 
+canvas.addEventListener('mousemove', game.onMouse);
+
+game.start();
--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-import { ctx, brickHeight, brickWidth } from './app.js';
+import { ctx, canvasWidth, canvasHeight, brickHeight, brickWidth, padHeight, padWidth, mouse, } from './app.js';
 
 
 export const game = {
@@ -6,14 +6,32 @@ export const game = {
         ctx.fillStyle = color;
         ctx.fillRect(x, y, brickWidth, brickHeight);
     },
-    pad: function (x, y) {
-        ctx.fillStyle = game.genColor();
-        ctx.fillRect(x, y, padWidth, padHeight);
+    pad: function (x) {
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(x - padWidth / 2, canvasHeight - 50, padWidth, padHeight);
     },
     genColor: function () {
-        const colors = ['blue', 'purple', 'velvet', 'green', 'orange', 'yellow']
+        const colors = ['blue', 'purple', 'velvet', 'lightgreen', 'orange', 'yellow']
         let rng = Math.floor(Math.random() * 5);
         return colors[rng];
+    },
+    render: function () {
+        game.clear();
+        game.pad(mouse.x);
+    },
+    onMouse: function (e) {
+        mouse.x = e.offsetX;
+        mouse.y = e.offsetY;
+    },
+    clear: function () {
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    },
+    main: function () {
+        game.render();
+        requestAnimationFrame(game.main);
+    },
+    start: function () {
+        requestAnimationFrame(game.main);
     },
 
 };
